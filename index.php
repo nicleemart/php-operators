05dc301c8948b816ddca9ebd0f0a0320d3e7738b
Nikki <nicleemart@gmail.com>
2016-11-16 18:01:47 -0600

    started html

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+function calculator () {
+	$numOne = $_GET(numOne);
+	$operator = "addition";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form>
+		<input type="text">
+	</form>	
+
+</body>
+</html>
