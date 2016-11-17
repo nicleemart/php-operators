711c1efbb068a1f6f29f8864e4355143fe602677
Nikki <nicleemart@gmail.com>
2016-11-16 18:53:27 -0600

    completed calculator

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 
 function calculator () {
-	$numOne = $_GET(numOne);
-	$operator = "addition";
+	$numOne = $_GET["numOne"];
+	$numTwo = $_GET["numTwo"];
+	$operator = $_GET["operator"];
+
+	if ($operator == "+") {
+		echo $numOne + $numTwo;
+	}
+	elseif ($operator == "-") {
+		echo $numOne - $numTwo;
+	}
+	elseif ($operator == "*") {
+		echo $numOne * $numTwo;
+	}
+	elseif ($operator == "/") {
+		echo $numOne / $numTwo;
+	}
 }
 
 ?>
@@ -15,8 +29,13 @@ function calculator () {
 <body>
 
 	<form>
-		<input type="text">
-	</form>	
+		<input type="text" name="numOne">
+		<input type="text" name="operator">
+		<input type="text" name="numTwo">
+		<input type="submit">
+	</form>
+
+	<?php calculator(); ?>	
 
 </body>
 </html>
